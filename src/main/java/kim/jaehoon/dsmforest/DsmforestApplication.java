8a8aa74ca7a853101d14c3cb6478af51899e9313
by09115 <by09115@outlook.kr>
2019-04-08 10:22:28 +0900

    [CREATE] Created Project Structure for Spring Boot

---
+++ src/main/java/kim/jaehoon/dsmforest/DsmforestApplication.java
@@ -0,0 +1,13 @@
+package kim.jaehoon.dsmforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmforestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmforestApplication.class, args);
+    }
+
+}
