5ce02aa1ebb72d92ddf3ed0522077c9e35915919
Jaehoon Kim <by09115@outlook.kr>
2019-09-23 16:19:46 +0900

    DELETE: delete before files

--- src/main/java/kim/jaehoon/dsmforest/common/Exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package kim.jaehoon.dsmforest.common.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code=HttpStatus.FORBIDDEN, reason="cannot find match admin info.")
-public class UnauthorizedException extends RuntimeException {
-}
