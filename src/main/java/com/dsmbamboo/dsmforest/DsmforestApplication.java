40ace002efd1d70252d407ae0e96b86d7c685a3e
Jaehoon Kim <by09115@outlook.kr>
2019-09-23 16:29:16 +0900

    ADD: 프로젝트 구조 초기화

---
+++ src/main/java/com/dsmbamboo/dsmforest/DsmforestApplication.java
@@ -0,0 +1,13 @@
+package com.dsmbamboo.dsmforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmforestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmforestApplication.class, args);
+    }
+
+}
