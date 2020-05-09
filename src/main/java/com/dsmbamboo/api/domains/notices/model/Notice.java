e952a2a7218303b5d41b565eff195f0e3049edb6
Jaehoon Kim <by09115@outlook.kr>
2020-05-10 00:05:09 +0900

    [FIX] user column

--- src/main/java/com/dsmbamboo/api/domains/notices/model/Notice.java
+++ src/main/java/com/dsmbamboo/api/domains/notices/model/Notice.java
@@ -1,6 +1,7 @@
 package com.dsmbamboo.api.domains.notices.model;
 
 import com.dsmbamboo.api.domains.posts.model.Category;
+import com.dsmbamboo.api.domains.users.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
