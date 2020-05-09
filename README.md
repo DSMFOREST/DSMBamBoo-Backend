da597e6322306ba13cfc728851575a29c88e956d
Jaehoon Kim <by09115@outlook.kr>
2020-05-09 20:52:31 +0900

    [INIT] new project for refactoring

--- README.md
+++
@@ -1,3 +0,0 @@
-# DSMBamBoo-Backend 
-[](https://img.shields.io/website?down_color=lightgrey&down_message=offline&style=plastic&up_color=blue&up_message=online&url=https%3A%2F%2Fapi.dsmbamboo.com%2Fapi%2Fv1%2Factuator%2Fhealth)
-대덕소프트웨어마이스터고등학교 대나무숲 Backend API
--- src/main/java/com/dsmbamboo/api/ApiApplication.java
+++ src/main/java/com/dsmbamboo/api/ApiApplication.java
@@ -1,13 +1,13 @@
-package com.dsmbamboo.dsmforest;
+package com.dsmbamboo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmforestApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DsmforestApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
