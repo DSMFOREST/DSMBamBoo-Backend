edaecf3820a164ae6f1dd6f2906b312198961929
Jaehoon Kim <by09115@outlook.kr>
2020-05-11 23:29:36 +0900

    [ADD] custom JWT filter with Spring Security

---
+++ src/main/java/com/dsmbamboo/api/domains/users/security/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.dsmbamboo.api.domains.users.security;
+
+import com.dsmbamboo.api.domains.users.model.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserPrincipal::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Username:" + username + "not found."));
+    }
+}
