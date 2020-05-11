9c6c78f0aea6080a0b85bb2ab37f2f2c8b685c6a
Jaehoon Kim <by09115@outlook.kr>
2020-05-11 23:31:39 +0900

    [ADD] endpoint of user authentication and authorization

--- src/main/java/com/dsmbamboo/api/domains/users/dto/UserSignInDTO.java
+++ src/main/java/com/dsmbamboo/api/domains/users/dto/UserSignInDTO.java
@@ -1,10 +1,15 @@
 package com.dsmbamboo.api.domains.users.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSignInDTO {
 
     @NotEmpty
@@ -12,5 +17,9 @@ public class UserSignInDTO {
 
     @NotEmpty
     private String password;
+
+    public UsernamePasswordAuthenticationToken getAuthenticationToken() {
+        return new UsernamePasswordAuthenticationToken(username, password);
+    }
 
 }
