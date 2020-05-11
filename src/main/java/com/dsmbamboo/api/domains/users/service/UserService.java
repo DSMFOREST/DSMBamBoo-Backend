ea1c754e69bc47c6e5a13951ab78c8d5dad5a14b
Jaehoon Kim <by09115@outlook.kr>
2020-05-11 23:28:05 +0900

    [ADD] service layer of user

---
+++ src/main/java/com/dsmbamboo/api/domains/users/service/UserService.java
@@ -0,0 +1,16 @@
+package com.dsmbamboo.api.domains.users.service;
+
+import com.dsmbamboo.api.domains.users.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface UserService {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndRawPassword(String username, String password);
+
+    User save(User user);
+
+}
