c429a6d610fc13e22769742639abf227790fc82e
Jaehoon Kim <by09115@outlook.kr>
2020-05-21 22:11:52 +0900

    [DELETE] useless POJO

--- src/main/java/com/dsmbamboo/api/domains/posts/model/DraftRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dsmbamboo.api.domains.posts.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DraftRepository extends CrudRepository<Draft, Long> {
-}
