b288f9e5de005d62e112ac2a54ec944ebb4717f8
Jaehoon Kim <me@jaehoon.kim>
2020-06-02 09:57:35 +0900

    [DELETE] notice entities - Use category field instead of notice entity.

--- src/main/java/com/dsmbamboo/api/domains/notices/model/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dsmbamboo.api.domains.notices.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeRepository extends CrudRepository<Notice, Long> {
-}
