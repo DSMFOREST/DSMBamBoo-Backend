5a8f59584e40ac6b22aea0b3ddf861193679a504
LumpKim <me@jaehoon.kim>
2020-06-16 18:58:17 +0900

    [ADD] query methods

--- src/main/java/com/dsmbamboo/api/domains/posts/model/CategoryRepository.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/model/CategoryRepository.java
@@ -3,6 +3,14 @@ package com.dsmbamboo.api.domains.posts.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAll();
+    List<Category> findAllByIsAdminOnlyFalse();
+    Optional<Category> findByName(String name);
+
 }
