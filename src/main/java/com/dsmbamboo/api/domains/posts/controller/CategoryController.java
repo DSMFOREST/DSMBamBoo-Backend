4ea999194cd7d8ca1de8b03b90f3006cbc75df69
LumpKim <me@jaehoon.kim>
2020-06-18 22:28:07 +0900

    [ADD] CategoryResponse DTO

--- src/main/java/com/dsmbamboo/api/domains/posts/controller/CategoryController.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.dsmbamboo.api.domains.posts.controller;
 
+import com.dsmbamboo.api.domains.posts.dto.CategoryResponse;
 import com.dsmbamboo.api.domains.posts.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/categories")
@@ -16,8 +18,11 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<String> getCategories() {
-        return categoryService.getCategories();
+    public List<CategoryResponse> getCategories() {
+        return categoryService.getCategories()
+                .stream()
+                .map(CategoryResponse::new)
+                .collect(Collectors.toList());
     }
 
 }
