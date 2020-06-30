9aa4ee35e579deb99d0b9591cb9f962e80ecfd66
LumpKim <me@jaehoon.kim>
2020-06-30 23:16:13 +0900

    [FIX] JPA Auditing Column: username -> id

--- src/main/java/com/dsmbamboo/api/domains/commons/model/CommonAuditorAware.java
+++ src/main/java/com/dsmbamboo/api/domains/commons/model/CommonAuditorAware.java
@@ -1,6 +1,8 @@
 package com.dsmbamboo.api.domains.commons.model;
 
+import com.dsmbamboo.api.domains.users.security.UserPrincipal;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -9,8 +11,19 @@ public class CommonAuditorAware implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return Optional.ofNullable(username);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (isUnauthenticated(auth))
+            return Optional.empty();
+
+        if (auth.getPrincipal() instanceof UserPrincipal) {
+            return Optional.of(((UserPrincipal) auth.getPrincipal()).getId().toString());
+        } else {
+            return Optional.of(auth.getName());
+        }
+    }
+
+    private boolean isUnauthenticated(Authentication authentication) {
+        return authentication == null || !authentication.isAuthenticated();
     }
 
 }
