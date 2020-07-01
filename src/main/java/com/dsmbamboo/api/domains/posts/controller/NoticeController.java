05b28a2529f89347c80dd23e5057d8dd9806fc04
LumpKim <me@jaehoon.kim>
2020-07-01 13:24:02 +0900

    [ADD] get single notice endpoint

--- src/main/java/com/dsmbamboo/api/domains/posts/controller/NoticeController.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/controller/NoticeController.java
@@ -1,13 +1,17 @@
 package com.dsmbamboo.api.domains.posts.controller;
 
 import com.dsmbamboo.api.domains.posts.dto.NoticeResponse;
+import com.dsmbamboo.api.domains.posts.exception.ArticleNotFoundException;
 import com.dsmbamboo.api.domains.posts.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/notices")
@@ -18,7 +22,15 @@ public class NoticeController {
 
     @GetMapping
     public Page<NoticeResponse> findAll(Pageable pageable) {
-        return noticeService.findAll(pageable);
+        return noticeService.findAll(pageable)
+                .map(NoticeResponse::new);
+    }
+
+    @GetMapping("/{noticeId}")
+    public NoticeResponse findByNoticeId(@PathVariable @Valid Long noticeId) {
+        return noticeService.findByNoticeId(noticeId)
+                .map(NoticeResponse::new)
+                .orElseThrow(ArticleNotFoundException::new);
     }
 
 }
