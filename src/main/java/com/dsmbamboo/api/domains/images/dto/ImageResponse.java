fed6f45943ec36c271f3724279a2ff0e7859871c
LumpKim <me@jaehoon.kim>
2020-07-01 11:13:54 +0900

    [FIX] include url prefix in ImageResponse.url

--- src/main/java/com/dsmbamboo/api/domains/images/dto/ImageResponse.java
+++ src/main/java/com/dsmbamboo/api/domains/images/dto/ImageResponse.java
@@ -1,6 +1,7 @@
 package com.dsmbamboo.api.domains.images.dto;
 
 import com.dsmbamboo.api.domains.images.model.Image;
+import com.fasterxml.jackson.annotation.JsonGetter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ImageResponse {
 
+    private static final String IMAGE_URL_PREFIX = "https://img.dsmbamboo.com/";
+
     private Long id;
     private String url;
+
+    @JsonGetter("url")
+    public String getUrl() {
+        return IMAGE_URL_PREFIX.concat(url);
+    }
 
     public ImageResponse(Image image) {
         this.id = image.getId();
