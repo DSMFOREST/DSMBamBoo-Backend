4a184d6ec57d120795ed733f432978052367d14a
JaeHoon Kim <me@jaehoon.kim>
2020-07-03 10:58:20 +0900

    Update Device Token: 디바이스 토큰 갱신 (#22)
    * [FIX] token column's max length
    
    * [FIX] Article.category to Article.categories
    
    * [ADD] Category.isAdminOnly column
    
    * [FIX] apply `@RequiredArgsConstructor`
    
    * [ADD] issuer claim in JWT
    
    * [ADD] query methods
    
    * [ADD] CategoryService with impl
    
    * [ADD] CategoryController
    
    * [DELETE] useless util
    
    * [FIX] OneToMany to ManyToMany in Article.categories field
    
    * [ADD] Article repo & service
    
    * [ADD] Notice service with impl
    
    * [ADD] Notice response DTO
    
    * [ADD] Notice controller
    
    * [FIX] Article.publishedId equal to NoticeResponse.id
    
    * [DELETE] Image.sequence_id field
    
    * [FIX] exclude timezone settings in Jackson annotations
    
    * [ADD] `approved_at` field on NoticeResponse
    
    * [ADD] CategoryResponse DTO (#18)
    
    * [ADD] CORS Configs
    
    * [ADD] Custom Security Filter that handling occurred exception in another filter.
    
    * [ADD] query method: findAllByUsernameAndRefreshToken()
    
    * [FIX] JwtTokenProvider.validateToken() -> .validateAccessToken()
    
    * [ADD] JwtTokenProvider.validateRefreshTokenAndGetIdentity()
    
    * [ADD] refresh token endpoint
    
    * [FIX] clean code AuthServiceImpl.userSignIn
    [ADD] AuthServiceImpl.refreshToken
    
    * [ADD] method to get username in spring security
    
    * [ADD] endpoint that update device token

---
+++ src/main/java/com/dsmbamboo/api/domains/users/security/AuthenticationFacadeImpl.java
@@ -0,0 +1,20 @@
+package com.dsmbamboo.api.domains.users.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacadeImpl implements AuthenticationFacade {
+
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getAuthentication().getName();
+    }
+
+}
