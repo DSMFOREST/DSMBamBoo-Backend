5f6229b1a40cecbfcade16b3b1254b08e7586328
LumpKim <me@jaehoon.kim>
2020-07-06 23:34:49 +0900

    [FIX] impl: ArticleService::create

--- src/main/java/com/dsmbamboo/api/domains/posts/service/ArticleService.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/ArticleService.java
@@ -1,5 +1,6 @@
 package com.dsmbamboo.api.domains.posts.service;
 
+import com.dsmbamboo.api.domains.posts.dto.CreateArticleRequest;
 import com.dsmbamboo.api.domains.posts.model.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,5 +11,7 @@ public interface ArticleService {
 
     Page<Article> findAllByCategories_Name(String categoryName, Pageable pageable);
     Optional<Article> findByCategories_NameAndPublishedId(String categoryName, Long publishedId);
+
+    Article create(CreateArticleRequest request, Long publishedId);
 
 }
--- src/main/java/com/dsmbamboo/api/domains/posts/service/ArticleServiceImpl.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/ArticleServiceImpl.java
@@ -1,7 +1,10 @@
 package com.dsmbamboo.api.domains.posts.service;
 
+import com.dsmbamboo.api.domains.images.model.ImageRepository;
+import com.dsmbamboo.api.domains.posts.dto.CreateArticleRequest;
 import com.dsmbamboo.api.domains.posts.model.Article;
 import com.dsmbamboo.api.domains.posts.model.ArticleRepository;
+import com.dsmbamboo.api.domains.posts.model.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,8 @@ import java.util.Optional;
 public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final CategoryRepository categoryRepository;
+    private final ImageRepository imageRepository;
 
     @Override
     public Page<Article> findAllByCategories_Name(String categoryName, Pageable pageable) {
@@ -24,5 +29,20 @@ public class ArticleServiceImpl implements ArticleService {
     public Optional<Article> findByCategories_NameAndPublishedId(String categoryName, Long publishedId) {
         return articleRepository.findAllByCategories_NameAndPublishedId(categoryName, publishedId);
     }
-  
+
+    @Override
+    public Article create(CreateArticleRequest request, Long publishedId) {
+        return articleRepository.save(Article.builder()
+                .id(0L)
+                .title(request.getTitle())
+                .content(request.getContent())
+                .categories(categoryRepository.findAllById(request.getCategories()))
+                .images(imageRepository.findAllById(request.getImages()))
+                .publishedId(publishedId)
+                .approver(null)
+                .isActive(true)
+                .facebookLink(null)
+                .build());
+    }
+
 }
