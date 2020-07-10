c8fa42699f6b85bd8361b0c0a3455fe09487d54a
LumpKim <me@jaehoon.kim>
2020-07-10 13:38:43 +0900

    [ADD] StudentQuestionRepository::findByRandomId

--- src/main/java/com/dsmbamboo/api/domains/questions/model/StudentQuestionRepository.java
+++ src/main/java/com/dsmbamboo/api/domains/questions/model/StudentQuestionRepository.java
@@ -1,8 +1,15 @@
 package com.dsmbamboo.api.domains.questions.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentQuestionRepository extends JpaRepository<StudentQuestion, Long> {
+
+    @Query(value = "SELECT * FROM student_question ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<StudentQuestion> findByRandomId();
+
 }
--- src/main/java/com/dsmbamboo/api/domains/questions/service/StudentQuestionService.java
+++ src/main/java/com/dsmbamboo/api/domains/questions/service/StudentQuestionService.java
@@ -3,8 +3,11 @@ package com.dsmbamboo.api.domains.questions.service;
 import com.dsmbamboo.api.domains.questions.dto.CreateQuestionRequest;
 import com.dsmbamboo.api.domains.questions.model.StudentQuestion;
 
+import java.util.Optional;
+
 public interface StudentQuestionService {
 
     StudentQuestion create(CreateQuestionRequest request);
+    Optional<StudentQuestion> findByRandomId();
 
 }
