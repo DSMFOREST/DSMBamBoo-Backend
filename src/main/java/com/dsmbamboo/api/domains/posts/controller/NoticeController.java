fa707eeeeb89b709188d8c11687f42d6295518f0
LumpKim <me@jaehoon.kim>
2020-07-11 23:40:34 +0900

    [FIX] handle NOT_FOUND exception on controller layer

--- src/main/java/com/dsmbamboo/api/domains/posts/controller/NoticeController.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/controller/NoticeController.java
@@ -4,6 +4,7 @@ import com.dsmbamboo.api.domains.posts.dto.CreateArticleRequest;
 import com.dsmbamboo.api.domains.posts.dto.NoticeResponse;
 import com.dsmbamboo.api.domains.posts.exception.ArticleNotFoundException;
 import com.dsmbamboo.api.domains.posts.service.NoticeService;
+import com.dsmbamboo.api.domains.users.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -37,7 +38,9 @@ public class NoticeController {
     @Secured("ROLE_ADMIN")
     @ResponseStatus(HttpStatus.CREATED)
     public NoticeResponse create(@RequestBody @Valid CreateArticleRequest request) {
-        return noticeService.create(request);
+        return noticeService.create(request)
+                .map(NoticeResponse::new)
+                .orElseThrow(UserNotFoundException::new);
     }
 
 }
