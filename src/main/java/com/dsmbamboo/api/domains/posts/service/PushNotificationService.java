67d583e01cfa165eea750a39920b05c0af3cfff1
LumpKim <me@jaehoon.kim>
2020-07-13 16:55:45 +0900

    [FIX] add: PushNotificationService::sendToSingleDevice

--- src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationService.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface PushNotificationService {
 
+    void sendToSingleDevice(String deviceToken, String title, String context);
     void sendToMultipleDevices(List<String> deviceTokens, String title, String content);
 
 }
--- src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationServiceImpl.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationServiceImpl.java
@@ -1,9 +1,6 @@
 package com.dsmbamboo.api.domains.posts.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.MulticastMessage;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,13 +9,26 @@ import java.util.List;
 public class PushNotificationServiceImpl implements PushNotificationService {
 
     @Override
+    public void sendToSingleDevice(String deviceToken, String title, String context) {
+        Notification notification = new Notification(title, context);
+        Message message = Message.builder()
+                .setNotification(notification)
+                .setToken(deviceToken)
+                .build();
+        try {
+            FirebaseMessaging.getInstance().send(message);
+        } catch (FirebaseMessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public void sendToMultipleDevices(List<String> deviceTokens, String title, String content) {
         Notification notification = new Notification(title, content);
         MulticastMessage message = MulticastMessage.builder()
                 .setNotification(notification)
                 .addAllTokens(deviceTokens)
                 .build();
-
         try {
             FirebaseMessaging.getInstance().sendMulticast(message);
         } catch (FirebaseMessagingException e) {
