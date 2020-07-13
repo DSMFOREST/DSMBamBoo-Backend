a5467bd2b1f901c41353a6883a9fc4ab57475337
LumpKim <me@jaehoon.kim>
2020-07-13 23:53:16 +0900

    [FIX] add: helpful method to send push notifications

--- src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationService.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationService.java
@@ -6,5 +6,8 @@ public interface PushNotificationService {
 
     void sendToSingleDevice(String deviceToken, String title, String context);
     void sendToMultipleDevices(List<String> deviceTokens, String title, String content);
+    void sendToAdministrators(String title, String content);
+    void sendToAnonymousUsers(String title, String content);
+    void sendToSingleDeviceByUserId(Long userId, String title, String content);
 
 }
--- src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationServiceImpl.java
+++ src/main/java/com/dsmbamboo/api/domains/posts/service/PushNotificationServiceImpl.java
@@ -1,12 +1,17 @@
 package com.dsmbamboo.api.domains.posts.service;
 
+import com.dsmbamboo.api.domains.users.service.UserService;
 import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PushNotificationServiceImpl implements PushNotificationService {
+
+    private final UserService userService;
 
     @Override
     public void sendToSingleDevice(String deviceToken, String title, String context) {
@@ -18,7 +23,8 @@ public class PushNotificationServiceImpl implements PushNotificationService {
         try {
             FirebaseMessaging.getInstance().send(message);
         } catch (FirebaseMessagingException e) {
-            e.printStackTrace();
+            if (!e.getErrorCode().equals("404"))
+                e.printStackTrace();
         }
     }
 
@@ -32,8 +38,27 @@ public class PushNotificationServiceImpl implements PushNotificationService {
         try {
             FirebaseMessaging.getInstance().sendMulticast(message);
         } catch (FirebaseMessagingException e) {
-            e.printStackTrace();
+            if (!e.getErrorCode().equals("404"))
+                e.printStackTrace();
         }
+    }
+
+    @Override
+    public void sendToAdministrators(String title, String content) {
+        List<String> deviceTokens = userService.findAllDeviceTokensByRole("ROLE_ADMIN");
+        this.sendToMultipleDevices(deviceTokens, title, content);
+    }
+
+    @Override
+    public void sendToAnonymousUsers(String title, String content) {
+        List<String> deviceTokens = userService.findAllDeviceTokensByRole("ROLE_ANONYMOUS");
+        this.sendToMultipleDevices(deviceTokens, title, content);
+    }
+
+    @Override
+    public void sendToSingleDeviceByUserId(Long userId, String title, String content) {
+        userService.findById(userId)
+                .ifPresent(user -> this.sendToSingleDevice(user.getDeviceToken(), title, content));
     }
 
 }
