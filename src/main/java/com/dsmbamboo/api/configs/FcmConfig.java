46c49aa7fdebe2735abf8a94c2ec5edc6ef53118
LumpKim <me@jaehoon.kim>
2020-07-14 12:51:28 +0900

    [FIX] set firebase credential by json

--- src/main/java/com/dsmbamboo/api/configs/FcmConfig.java
+++ src/main/java/com/dsmbamboo/api/configs/FcmConfig.java
@@ -1,15 +1,26 @@
 package com.dsmbamboo.api.configs;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FcmConfig {
 
     @Bean
-    public FirebaseApp getFirebaseApp() {
-        return FirebaseApp.initializeApp();
+    public FirebaseApp getFirebaseApp() throws IOException {
+        InputStream resourceStream = new ClassPathResource("firebase-adminsdk-credential.json").getInputStream();
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resourceStream);
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+        return FirebaseApp.initializeApp(options);
     }
 
 }
